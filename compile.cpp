b66dc37a5672042b477e6bf96f6c3108976e599e
Yasir Arfat <38040354+coddleflop@users.noreply.github.com>
2021-09-30 12:12:58 +0600

    named output

--- compile.cpp
+++ compile.cpp
@@ -6,56 +6,85 @@
 using namespace std;
 bool exists_test(const string name)
 {
-        ifstream f(name.c_str());
-        return f.good();
+    ifstream f(name.c_str());
+    return f.good();
 }
 string current_working_directory()
 {
-        char *cwd = _getcwd(0, 0);
-        string working_directory(cwd);
-        free(cwd);
-        return working_directory;
+    char *cwd = _getcwd(0, 0);
+    string working_directory(cwd);
+    free(cwd);
+    return working_directory;
 }
 int main(int argc, char const *argv[])
 {
-        string file = current_working_directory();
-        file += "\\";
-        //string file = argv[0];
-        //file.erase(file.length() - 11, 11);
-        string Command = "g++ ";
-        bool flag = false;
+    string file = current_working_directory();
+    file += "\\";
+    //string file = argv[0];
+    //file.erase(file.length() - 11, 11);
+    string Command = "g++ ";
+    string fileWoExt;
+    bool flag = false;
 
-        if (argc < 2)
+    if (argc < 2)
+    {
+        string s;
+        printf("Enter File Name:");
+        getline(cin, s);
+        file += s;
+        fileWoExt = s;
+    }
+    else
+    {
+        file += argv[1];
+        fileWoExt = argv[1];
+    }
+    if (exists_test(file + ".cpp"))
+    {
+        file += ".cpp";
+    }
+    else if (exists_test(file + ".c"))
+    {
+        file += ".c";
+    }
+    else if (exists_test(file))
+    {
+        if (file.find_last_of("."))
         {
-                string s;
-                printf("Enter File Name:");
-                getline(cin, s);
-                file += s;
+            string s = file.substr(file.find_last_of("."));
+            if (!(s == ".c" || s == ".cpp"))
+            {
+                printf("File type not valid!\nCouldn't initiate compilation.\n");
+                return 0;
+            }
         }
-        else
-        {
-                file += argv[1];
-        }
-
-        if (exists_test(file + ".cpp"))
-        {
-                Command += file + ".cpp";
-        }
-        else if (exists_test(file + ".c"))
-        {
-                Command += file + ".c";
-        }
-        else
-        {
-                if (!exists_test(file))
-                {
-                        printf("File not found!\nCouldn't initiate compilation.\n");
-                        return 0;
-                }
-                Command += file;
-        }
-        system(Command.data());
-        //system("clear");
-        system("a");
+    }
+    else if (!exists_test(file))
+    {
+        printf("File not found!\nCouldn't initiate compilation.\n");
         return 0;
+    };
+
+    if (fileWoExt.find_last_of("."))
+        fileWoExt = fileWoExt.substr(0, fileWoExt.find_last_of("."));
+
+    Command += " -o " + fileWoExt + " " + file + " && echo $?";
+    cout << "#Compiling " << fileWoExt << "....." << endl;
+    int b = system(Command.data());
+    if (!b)
+    {
+        cout << "Compilation successful" << endl
+             << "Running " << fileWoExt << ".exe..." << endl;
+        system("clear");
+        system(fileWoExt.data());
+    }
+    else
+    {
+        cout << endl
+             << endl
+             << "#Failed." << endl
+             << "Fix the errors above and compile again." << endl;
+    }
+
+    return 0;
 }
